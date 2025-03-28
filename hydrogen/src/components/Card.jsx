db654acd581ead7db30fb0302ff32c1d312a253a
Arunabh Das <arunabhdas@gmail.com>
2025-03-28 04:25:09 -0400

    Added Card component

---
+++ hydrogen/src/components/Card.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+function Card() {
+  return (
+    <div className="card">
+        <h1>Card Name</h1>
+        <p>Card Description</p>
+    </div>
+  )
+}
+
+export default Card
